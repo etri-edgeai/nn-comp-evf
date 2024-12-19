b551cdc85924ec2b4de99f0cc9bfde1538cdd973
Junyong Park <junyong300@gmail.com>
2024-12-19 16:07:55 +0900

    feat: add docs

--- edgeai/template/project/optimizations/weight_prune.py
+++ edgeai/template/project/optimizations/weight_prune.py
@@ -4,7 +4,13 @@ import torch.nn.utils.prune as prune
 
 class WeightPruning:
     def __init__(self, model, amount=0.2):
+        """
+        Initializes weight pruning for the given model.
 
+        Parameters:
+        model (torch.nn.Module): The PyTorch model to be pruned.
+        amount (float): The proportion of weights to prune (default is 0.2).
+        """
         self.model = model
         self.amount = amount
     
