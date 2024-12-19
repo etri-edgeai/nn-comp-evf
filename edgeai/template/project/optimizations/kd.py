d9629eb088053745bd8a9eae431450e6af5af60a
Junyong Park <junyong300@gmail.com>
2024-12-19 16:10:00 +0900

    feat: kd

--- edgeai/template/project/optimizations/kd.py
+++ edgeai/template/project/optimizations/kd.py
@@ -1,2 +1,10 @@
 import torch
 import torch.nn.functional as F
+
+
+class KnowledgeDistillation:
+    def __init__(self, teacher, student, temperature=2.0, alpha=0.5):
+        self.teacher = teacher
+        self.student = student
+        self.temperature = temperature
+        self.alpha = alpha
