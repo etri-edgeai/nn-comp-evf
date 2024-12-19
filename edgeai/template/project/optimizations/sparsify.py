b5557ee47209884f287e508a759695d3bda4d163
Junyong Park <junyong300@gmail.com>
2024-12-19 16:12:15 +0900

    feat: sparsify

--- edgeai/template/project/optimizations/sparsify.py
+++ edgeai/template/project/optimizations/sparsify.py
@@ -2,3 +2,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.utils.prune as prune
+
+class Sparsify:
+    def __init__(self, model, sparsity=0.5):
+
+        self.model = model
+        self.sparsity = sparsity
+
+    def apply(self):
+        for module in self.model.modules():
+            if isinstance(module, nn.Linear):
+                prune.random_unstructured(module, name='weight', amount=self.sparsity)
+        return self.model
