dc8e120047d502cabe9dc1477bb79843a4cb7cf2
Junyong Park <junyong300@gmail.com>
2024-12-19 16:07:25 +0900

    feat: add weight prune

--- edgeai/template/project/optimizations/weight_prune.py
+++ edgeai/template/project/optimizations/weight_prune.py
@@ -25,3 +25,15 @@ class WeightPruning:
             if isinstance(module, (nn.Conv2d, nn.Linear)):
                 prune.l1_unstructured(module, name='weight', amount=self.amount)
         return self.model
+    
+    def unprune(self):
+        """
+        Removes pruning masks from the model.
+
+        Returns:
+        torch.nn.Module: The unpruned model.
+        """
+        for module in self.model.modules():
+            if isinstance(module, (nn.Conv2d, nn.Linear)) and hasattr(module, 'weight_mask'):
+                prune.remove(module, 'weight')
+        return self.model
