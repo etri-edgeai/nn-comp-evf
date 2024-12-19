d97306aa04ccf8da964f089e04babc62632296bd
Junyong Park <junyong300@gmail.com>
2024-12-19 16:10:17 +0900

    feat: kd

--- edgeai/template/project/optimizations/kd.py
+++ edgeai/template/project/optimizations/kd.py
@@ -8,3 +8,12 @@ class KnowledgeDistillation:
         self.student = student
         self.temperature = temperature
         self.alpha = alpha
+
+    def loss_fn(self, teacher_logits, student_logits, target):
+        distillation_loss = F.kl_div(
+            F.log_softmax(student_logits / self.temperature, dim=1),
+            F.softmax(teacher_logits / self.temperature, dim=1),
+            reduction='batchmean',
+        ) * (self.temperature ** 2)
+        student_loss = F.cross_entropy(student_logits, target)
+        return self.alpha * distillation_loss + (1 - self.alpha) * student_loss
