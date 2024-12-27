9b52d21cbeb23ac7918f318099a8b53534c9adda
Junyong Park <junyong300@gmail.com>
2024-12-27 11:45:56 +0900

    fix deploy

--- static/src/deploy.js
+++ static/src/deploy.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
+    // =================================================
+    // GLOBAL STATE
+    // =================================================
+    let selectedFilePath = null;        // path of file user clicked in tree
+    let chosenDeployMethod = null;      // 'scp' or 'ftp' from button
+    
 });
